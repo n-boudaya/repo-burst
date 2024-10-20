86cd52eea5cbfa6f937839964ffcc356c7fd0a22
nboudaya <nboudaya@uni-muenster.de>
2024-10-20 17:17:56 +0200

    Added different implementations of sunburst graph (working)

--- bundle.js
+++ bundle.js
@@ -1,90 +1,156 @@
 (function (d3) {
-  'use strict';
+    'use strict';
 
-  function _interopNamespaceDefault(e) {
-    var n = Object.create(null);
-    if (e) {
-      Object.keys(e).forEach(function (k) {
-        if (k !== 'default') {
-          var d = Object.getOwnPropertyDescriptor(e, k);
-          Object.defineProperty(n, k, d.get ? d : {
-            enumerable: true,
-            get: function () { return e[k]; }
-          });
+    function _interopNamespaceDefault(e) {
+        var n = Object.create(null);
+        if (e) {
+            Object.keys(e).forEach(function (k) {
+                if (k !== 'default') {
+                    var d = Object.getOwnPropertyDescriptor(e, k);
+                    Object.defineProperty(n, k, d.get ? d : {
+                        enumerable: true,
+                        get: function () { return e[k]; }
+                    });
+                }
+            });
         }
-      });
+        n.default = e;
+        return Object.freeze(n);
     }
-    n.default = e;
-    return Object.freeze(n);
-  }
 
-  var d3__namespace = /*#__PURE__*/_interopNamespaceDefault(d3);
+    var d3__namespace = /*#__PURE__*/_interopNamespaceDefault(d3);
 
-  d3__namespace.json("flare-2.json").then(function (flare) {
-    const chart = Sunburst2(flare);
-    console.log(chart);
-    const output = d3__namespace.select('body').append('svg').append('chart');
-    console.log(output);
-  });
+    d3__namespace.json("fileprocout.json").then(function (data) {
 
-  console.log("aeiou");
+        const svg = d3__namespace.select("body").append("svg").attr('width', window.innerHeight).attr('height', window.innerHeight);
 
-  function Sunburst2(data){
-      // Specify the chart’s colors and approximate radius (it will be adjusted at the end).
-      const color = d3__namespace.scaleOrdinal(d3__namespace.quantize(d3__namespace.interpolateRainbow, data.children.length + 1));
-      const radius = 928 / 2;
-    
-      // Prepare the layout.
-      const partition = data => d3__namespace.partition()
-        .size([2 * Math.PI, radius])
-      (d3__namespace.hierarchy(data)
-        .sum(d => d.value)
-        .sort((a, b) => b.value - a.value));
-    
-      const arc = d3__namespace.arc()
-        .startAngle(d => d.x0)
-        .endAngle(d => d.x1)
-        .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-        .padRadius(radius / 2)
-        .innerRadius(d => d.y0)
-        .outerRadius(d => d.y1 - 1);
-    
-      const root = partition(data);
-    
-      // Create the SVG container.
-      const svg = d3__namespace.create("svg");
-    
-      // Add an arc for each element, with a title for tooltips.
-      const format = d3__namespace.format(",d");
-      svg.append("g")
-          .attr("fill-opacity", 0.6)
-        .selectAll("path")
-        .data(root.descendants().filter(d => d.depth))
-        .join("path")
-          .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
-          .attr("d", arc)
-        .append("title")
-          .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
-    
-      // Add a label for each element.
-      svg.append("g")
-          .attr("pointer-events", "none")
-          .attr("text-anchor", "middle")
-          .attr("font-size", 10)
-          .attr("font-family", "sans-serif")
-        .selectAll("text")
-        .data(root.descendants().filter(d => d.depth && (d.y0 + d.y1) / 2 * (d.x1 - d.x0) > 10))
-        .join("text")
-          .attr("transform", function(d) {
+        svg.node().appendChild(SunburstZoom(data));
+
+    });
+
+    function SunburstZoom(data) {
+            // Specify the chart’s dimensions.
+            const width = 928;
+            const height = width;
+            const radius = width / 6;
+
+            // Create the color scale.
+            const color = d3__namespace.scaleOrdinal(d3__namespace.quantize(d3__namespace.interpolateRainbow, data.children.length + 1));
+
+            // Compute the layout.
+            const hierarchy = d3__namespace.hierarchy(data)
+                .sum(d => d.value)
+                .sort((a, b) => b.value - a.value);
+            const root = d3__namespace.partition()
+                .size([2 * Math.PI, hierarchy.height + 1])
+                (hierarchy);
+            root.each(d => d.current = d);
+
+            // Create the arc generator.
+            const arc = d3__namespace.arc()
+                .startAngle(d => d.x0)
+                .endAngle(d => d.x1)
+                .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+                .padRadius(radius * 1.5)
+                .innerRadius(d => d.y0 * radius)
+                .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1));
+
+            // Create the SVG container.
+            const svg = d3__namespace.create("svg")
+                .attr("viewBox", [-width / 2, -height / 2, width, width])
+                .style("font", "10px sans-serif");
+
+            // Append the arcs.
+            const path = svg.append("g")
+                .selectAll("path")
+                .data(root.descendants().slice(1))
+                .join("path")
+                .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+                .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+                .attr("pointer-events", d => arcVisible(d.current) ? "auto" : "none")
+
+                .attr("d", d => arc(d.current));
+
+            // Make them clickable if they have children.
+            path.filter(d => d.children)
+                .style("cursor", "pointer")
+                .on("click", clicked);
+
+            const format = d3__namespace.format(",d");
+            path.append("title")
+                .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+
+            const label = svg.append("g")
+                .attr("pointer-events", "none")
+                .attr("text-anchor", "middle")
+                .style("user-select", "none")
+                .selectAll("text")
+                .data(root.descendants().slice(1))
+                .join("text")
+                .attr("dy", "0.35em")
+                .attr("fill-opacity", d => +labelVisible(d.current))
+                .attr("transform", d => labelTransform(d.current))
+                .text(d => d.data.name);
+
+            const parent = svg.append("circle")
+                .datum(root)
+                .attr("r", radius)
+                .attr("fill", "none")
+                .attr("pointer-events", "all")
+                .on("click", clicked);
+
+            // Handle zoom on click.
+            function clicked(event, p) {
+            parent.datum(p.parent || root);
+
+            root.each(d => d.target = {
+                x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+                x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+                y0: Math.max(0, d.y0 - p.depth),
+                y1: Math.max(0, d.y1 - p.depth)
+            });
+
+            const t = svg.transition().duration(750);
+
+            // Transition the data on all arcs, even the ones that aren’t visible,
+            // so that if this transition is interrupted, entering arcs will start
+            // the next transition from the desired position.
+            path.transition(t)
+                .tween("data", d => {
+                    const i = d3__namespace.interpolate(d.current, d.target);
+                    return t => d.current = i(t);
+                })
+                .filter(function(d) {
+                    return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+                })
+                .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+                .attr("pointer-events", d => arcVisible(d.target) ? "auto" : "none")
+
+                .attrTween("d", d => () => arc(d.current));
+
+            label.filter(function(d) {
+                return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+            }).transition(t)
+                .attr("fill-opacity", d => +labelVisible(d.target))
+                .attrTween("transform", d => () => labelTransform(d.current));
+        }
+
+        function arcVisible(d) {
+            return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+        }
+
+        function labelVisible(d) {
+            return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+        }
+
+        function labelTransform(d) {
             const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
-            const y = (d.y0 + d.y1) / 2;
+            const y = (d.y0 + d.y1) / 2 * radius;
             return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
-          })
-          .attr("dy", "0.35em")
-          .text(d => d.data.name);
-      
-      return svg.node(); 
-  }
+        }
+
+        return svg.node();
+    }
 
 })(d3);
 //# sourceMappingURL=bundle.js.map
